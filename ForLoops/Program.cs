d4e27054f6d34d1790631c7a548127ac97574e2b
Nathan Strang <n.strang95@gmail.com>
2024-01-10 09:08:31 +0000

    Adding program code

---
+++ ForLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* For Loop will repeat for a finite amount of times 
+            * The below for loop will continue loop until index is <= 10 
+            * 'i' can also be displayed as 'index'
+            * There are 3 statements to fill in
+            * First - declares an index(i) int
+            * Second - The condition for the loop
+            * Third - How much to increment or decrement by
+            * ++ will increment by 1, to increase this value change to i+=x 
+            * -- will decrement by 1, to decrease this value change to i-=x
+            */
+
+            for (int i = 1; i <= 10; i ++)        
+            { 
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
